505dd193ad3091e933c8eff2e1660a026f23d0aa
Nabin Rayamajhi <nabin.rm@gmail.com>
2021-02-16 01:25:12 +1100

    Site switchover - fix links

---
+++ js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// Goolge Custom Search
+(function () {
+    var cx = '014710413280270296793:dwpyhvpgqka';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
